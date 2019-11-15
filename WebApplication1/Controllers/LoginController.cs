3e15b251060f6c350a311c5df557c94ae876771d
Pavan Kishore Reddy <pkreddy@juniper.net>
2019-11-15 20:00:03 +0530

    GoogleAuthApi

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Login")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        public LoginController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        // GET api/login
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var authenticationProperties = _signInManager.ConfigureExternalAuthenticationProperties("Google", Url.Action(nameof(HandleExternalLogin)));
+            return Challenge(authenticationProperties, "Google");
+        }
+
+        // GET api/login/callback
+        [HttpGet("callback")]
+        public async Task<String> HandleExternalLogin()
+        {
+             return "Login Sucessfully";
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<IdentityDbContext>(options =>
+                        options.UseSqlite("Data Source=users.sqlite",
+                        sqliteOptions => sqliteOptions.MigrationsAssembly("GoogleSpaWeb")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                    .AddEntityFrameworkStores<IdentityDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignOutScheme = IdentityConstants.ApplicationScheme;
+            })
+            .AddGoogle("Google", options =>
+            {
+                options.CallbackPath = new PathString("/api/login/callback");
+                options.ClientId = "552388567033-2670ukited4emrhceoqgj80ekoc3denp.apps.googleusercontent.com";
+                options.ClientSecret = "rCIoyhFdNcc0gNV6Q3FR9tiR";
+                options.Events = new OAuthEvents
+                {
+                    OnRemoteFailure = (RemoteFailureContext context) =>
+                    {
+                        context.Response.Redirect("/home/denied");
+                        context.HandleResponse();
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
